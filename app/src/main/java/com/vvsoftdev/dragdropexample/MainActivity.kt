87e3e828393e36e606d5c619c2b0304fd0980451
VincentVInsideApp <vincent.vigie@insideapp.fr>
2023-10-07 15:54:12 +0200

    init

---
+++ app/src/main/java/com/vvsoftdev/dragdropexample/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.vvsoftdev.dragdropexample
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.vvsoftdev.compose_drag_drop_list.DragDropList
+import com.vvsoftdev.dragdropexample.ui.theme.DragDropExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            DragDropExampleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    var fruits by remember {
+                        mutableStateOf(
+                            listOf(
+                                "apple",
+                                "banana",
+                                "cherry",
+                                "date",
+                                "elderberry",
+                                "fig",
+                                "grape",
+                                "kiwi",
+                                "lemon",
+                                "mango",
+                                "orange",
+                                "papaya",
+                                "quince",
+                                "raspberry",
+                                "strawberry",
+                                "tangerine",
+                                "watermelon"
+                            )
+                        )
+                    }
+
+                    DragDropList(
+                        items = fruits,
+                        onMove = { from, to ->
+                            // Update the list using the mutable state variable
+                            val fromItem = fruits[from]
+                            val toItem = fruits[to]
+                            val newList = fruits.toMutableList()
+                            newList[from] = toItem
+                            newList[to] = fromItem
+                            fruits = newList // Update the mutable state
+                            Log.d("DRAG_N_DROP", "Move $to $from")
+                        }
+                    ) {
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                                .height(64.dp),
+                            elevation = 4.dp,
+                            backgroundColor = MaterialTheme.colors.primary,
+                            border = BorderStroke(1.dp, MaterialTheme.colors.secondary),
+                        ) {
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text(
+                                    it,
+                                    color = Color.White,
+                                    fontSize = 16.sp
+                                )
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ compose-drag-drop-list/src/main/java/com/vvsoftdev/compose_drag_drop_list/DragDropList.kt
@@ -0,0 +1,196 @@
+package com.vvsoftdev.compose_drag_drop_list
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.scrollBy
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+@Composable
+fun <T: Any> DragDropList(
+    items: List<T>,
+    onMove: (Int, Int) -> Unit,
+    modifier: Modifier = Modifier,
+    itemComposable: @Composable (item: T) -> Unit
+) {
+
+    val scope = rememberCoroutineScope()
+
+    var overscrollJob by remember { mutableStateOf<Job?>(null) }
+
+    val dragDropListState = rememberDragDropListState(onMove = onMove)
+
+    LazyColumn(
+        modifier = modifier
+            .pointerInput(Unit) {
+                detectDragGesturesAfterLongPress(
+                    onDrag = { change, offset ->
+                        change.consume()
+                        dragDropListState.onDrag(offset)
+
+                        if (overscrollJob?.isActive == true)
+                            return@detectDragGesturesAfterLongPress
+
+                        val checkOver = dragDropListState.checkForOverScroll()
+                        if(checkOver != 0f )
+                            checkOver.let { overscrollJob = scope.launch { dragDropListState.lazyListState.scrollBy(checkOver) } }
+                    },
+                    onDragStart = { offset -> dragDropListState.onDragStart(offset) },
+                    onDragEnd = { dragDropListState.onDragInterrupted() },
+                    onDragCancel = { dragDropListState.onDragInterrupted() }
+                )
+            },
+        state = dragDropListState.lazyListState
+    ) {
+        itemsIndexed(items) { index, item ->
+            val currentIndex = rememberUpdatedState(index)
+            Column(
+                modifier = Modifier
+                    .composed {
+                        val offsetOrNull =
+                            dragDropListState.elementDisplacement.takeIf {
+                                index == dragDropListState.currentIndexOfDraggedItem
+                            }
+                        val translationYaxis by animateFloatAsState(targetValue = offsetOrNull ?: 0f)
+                        Modifier
+                            .graphicsLayer {
+                                translationY = translationYaxis
+                            }
+                    }
+                    .background(MaterialTheme.colors.background, shape = RoundedCornerShape(4.dp))
+                    .fillMaxWidth()
+            ) {
+                itemComposable(item)
+            }
+        }
+    }
+}
+
+@Composable
+fun rememberDragDropListState(
+    lazyListState: LazyListState = rememberLazyListState(),
+    onMove: (Int, Int) -> Unit,
+): DragDropListState {
+    return remember { DragDropListState(lazyListState = lazyListState, onMove = onMove) }
+}
+
+class DragDropListState(
+    val lazyListState: LazyListState,
+    private val onMove: (Int, Int) -> Unit
+) {
+    var draggedDistance by mutableStateOf(0f)
+
+    // used to obtain initial offsets on drag start
+    var initiallyDraggedElement by mutableStateOf<LazyListItemInfo?>(null)
+
+    var currentIndexOfDraggedItem by mutableStateOf<Int?>(null)
+
+    private val initialOffsets: Pair<Int, Int>?
+        get() = initiallyDraggedElement?.let { Pair(it.offset, it.offsetEnd) }
+
+    val elementDisplacement: Float?
+        get() = currentIndexOfDraggedItem
+            ?.let { lazyListState.getVisibleItemInfoFor(absoluteIndex = it) }
+            ?.let { item -> (initiallyDraggedElement?.offset ?: 0f).toFloat() + draggedDistance - item.offset }
+
+    private val currentElement: LazyListItemInfo?
+        get() = currentIndexOfDraggedItem?.let {
+            lazyListState.getVisibleItemInfoFor(absoluteIndex = it)
+        }
+
+    private var overscrollJob by mutableStateOf<Job?>(null)
+
+    fun onDragStart(offset: Offset) {
+        lazyListState.layoutInfo.visibleItemsInfo
+            .firstOrNull { item -> offset.y.toInt() in item.offset..(item.offset + item.size) }
+            ?.also {
+                currentIndexOfDraggedItem = it.index
+                initiallyDraggedElement = it
+            }
+    }
+
+    fun onDragInterrupted() {
+        draggedDistance = 0f
+        currentIndexOfDraggedItem = null
+        initiallyDraggedElement = null
+        overscrollJob?.cancel()
+    }
+
+    fun onDrag(offset: Offset) {
+        draggedDistance += offset.y
+
+        initialOffsets?.let { (topOffset, bottomOffset) ->
+            val startOffset = topOffset + draggedDistance
+            val endOffset = bottomOffset + draggedDistance
+
+            currentElement?.let { hovered ->
+                lazyListState.layoutInfo.visibleItemsInfo
+                    .filterNot { item -> item.offsetEnd < startOffset || item.offset > endOffset || hovered.index == item.index }
+                    .firstOrNull { item ->
+                        val delta = startOffset - hovered.offset
+                        when {
+                            delta > 0 -> (endOffset > item.offsetEnd)
+                            else -> (startOffset < item.offset)
+                        }
+                    }
+                    ?.also { item ->
+                        currentIndexOfDraggedItem?.let { current -> onMove.invoke(current, item.index) }
+                        currentIndexOfDraggedItem = item.index
+                    }
+            }
+        }
+    }
+
+    fun checkForOverScroll(): Float {
+        return initiallyDraggedElement?.let {
+            val startOffset = it.offset + draggedDistance
+            val endOffset = it.offsetEnd + draggedDistance
+
+            return@let when {
+                draggedDistance > 0 -> (endOffset - lazyListState.layoutInfo.viewportEndOffset).takeIf { diff -> diff > 0 }
+                draggedDistance < 0 -> (startOffset - lazyListState.layoutInfo.viewportStartOffset).takeIf { diff -> diff < 0 }
+                else -> null
+            }
+        } ?: 0f
+    }
+}
+
+/*
+    LazyListItemInfo.index is the item's absolute index in the list
+
+    Based on the item's "relative position" with the "currently top" visible item,
+    this returns LazyListItemInfo corresponding to it
+*/
+fun LazyListState.getVisibleItemInfoFor(absoluteIndex: Int): LazyListItemInfo? {
+    return this.layoutInfo.visibleItemsInfo.getOrNull(absoluteIndex - this.layoutInfo.visibleItemsInfo.first().index)
+}
+
+/*
+  Bottom offset of the element in Vertical list
+*/
+val LazyListItemInfo.offsetEnd: Int
+    get() = this.offset + this.size
+
+/*
+   Moving element in the list
+*/
+fun <T> MutableList<T>.move(from: Int, to: Int) {
+    if (from == to)
+        return
+
+    val element = this.removeAt(from) ?: return
+    this.add(to, element)
+}
